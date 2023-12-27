8ee31a845749730fff535ccd10c06f9fcce174ab
Rizal Purnawan <104172029+rizalpurnawan23@users.noreply.github.com>
2023-12-27 13:50:08 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='abstrak_stats_toolkit',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[],
+    url='https://github.com/rizalpurnawan23/Abstrak-Statistical-Toolkit'
 )
