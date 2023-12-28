d23b8848e5577433bd5fa16348ef2c27c0b33e47
Rizal Purnawan <104172029+rizalpurnawan23@users.noreply.github.com>
2023-12-28 17:46:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='abstrak_stats_toolkit',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.23.5'
